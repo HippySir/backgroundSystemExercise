11dc87797506cf7ef981fce6db038115fdeb1af4
HippySir <737438933@qq.com>
2019-04-29 08:58:01 +0800

    商品分类页基本逻辑的完成

--- src/components/categorylist.vue
+++ src/components/categorylist.vue
@@ -6,9 +6,10 @@
     <el-alert title="注意：只允许为第三级分类设置相关参数" type="warning" show-icon class="remindLabel"></el-alert>
     <!-- 级联菜单 -->
     <span class="selectCategory">前选择商品的分类：</span>
-    <el-cascader :options="options" v-model="selectedOptions" @change="handleChange"></el-cascader>
+    <el-cascader :options="options" v-model="selectedOptions" @change="handleChange"  :props="props"></el-cascader>
     <template>
-      <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tabs v-model="activeName" @tab-click="handleClick" >
+         <el-button type="primary" class="addButton"  :disabled="changeover">{{changeWord}}</el-button>
         <el-tab-pane label="静态参数" name="first">
           <el-table :data="roleData" style="width: 100%" border>
             <el-table-column type="expand">
@@ -69,19 +70,62 @@ export default {
   data() {
     return {
       options: [],
-      selectedOptions: []
+      selectedOptions: [],
+      props: {
+        value: 'cat_id',
+        label: 'cat_name',
+        children: 'children',
+      },
+      activeName: 'first',
+      changeover: true,
+      changeWord: '静态属性',
+      changeCondition: 'only',
+      // 下部表格的相关的数据
+      roleData: [],
+      goodsId: 0,
     };
   },
   methods: {
+    // 将获取商品参数的代码封装成为函数
+    async getData () {
+       let resa = await this.$axios.get(`categories/${this.goodsId}/attributes`,{
+        params: {
+          sel:  this.changeCondition,
+        }
+      });
+      console.log(resa);
+    },
+    // tab栏点击时的函数
+    handleClick (tab,event) {
+      console.log(tab,event);
+      if(tab.label === '动态参数'){
+          this.changeWord = '添加动态参数';
+          this.changeCondition = 'many';
+           this.getData (); 
+      }else{
+          this.changeWord = '添加静态参数';
+          this.changeCondition = 'only';
+           this.getData (); 
+      }
+    },
     // 级联菜单的相关函数
-    handleChange() {},
-    // tab切换栏的相关函数
-    handleChange() {},
+   async handleChange(res) {
+      this.changeover = false;
+      this.goodsId = res[res.length - 1];
+      this.getData (); 
+     
+    },
     // 编辑的相关函数
     editRolesa() {},
     // 删除的相关函数
     isDelete() {}
-  }
+  },
+  async created() {
+    let res = await this.$axios.get(`categories`);
+    console.log(res);
+    this.options = res.data.data;
+
+  },
 };
 </script>
 
@@ -99,6 +143,9 @@ export default {
         }
         .selectCategory {
 
+        }
+        .addButton {
+          margin-bottom: 15px;
         }
     }
 </style>
